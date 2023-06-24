1c71fa7271eb45199067c390a5fca4d17e4fdb29
Vodes <48185123+Vodes@users.noreply.github.com>
2023-06-24 02:28:24 +0200

    Add SubFile extension with truncating function

--- vsmuxtools/__init__.py
+++ vsmuxtools/__init__.py
@@ -2,10 +2,11 @@ from . import utils, video, extension
 
 from .utils import *
 from .video import *
-from .extension import *
 
 from muxtools.main import *
 from muxtools.audio import *
 from muxtools.muxing import *
 from muxtools.subtitle import *
 from muxtools.utils import *
+
+from .extension import *
