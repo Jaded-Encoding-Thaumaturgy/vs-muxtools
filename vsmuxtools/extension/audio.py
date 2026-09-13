6a2ede1f09ce61aaf5cff10a3a357a5d3fc114b9
Vardë <ichunjo.le.terrible@gmail.com>
2026-09-13 17:16:47 +0200

    export_audionode: improve wav/w64 export logic
    - Add `w64` parameter
    - Implement automatic W64 detection based on data size (> 4GiB)
    - Support explicit extension-based detection
    - Ensure correct file extension assignment for output paths

--- vsmuxtools/extension/audio.py
+++ vsmuxtools/extension/audio.py
@@ -27,23 +27,40 @@ __all__ = ["do_audio", "encode_audio", "export_audionode"]
 
 
 @keep.running
-def export_audionode(node: vs.AudioNode, outfile: PathLike | None = None) -> Path:
+def export_audionode(node: vs.AudioNode, outfile: PathLike | None = None, w64: bool | None = None) -> Path:
     """
     Exports an audionode to a wav/w64 file.
 
     :param node:            Your audionode
     :param outfile:         Custom output path if any
+    :param w64:             Force Wave64 output or auto-detect based on file size/extension
 
     :return:                Returns path
     """
     from vstools.functions.render.progress import get_render_progress
 
-    if not outfile:
-        outfile = uniquify_path(Path(get_workdir(), "exported.wav"))
+    if outfile is not None:
+        outfile = ensure_path(outfile, export_audionode)
 
-    outfile = ensure_path(outfile, export_audionode)
+    if w64 is None:
+        if outfile is not None and outfile.suffix.lower() == ".w64":
+            w64 = True
+        else:
+            # Check if the resulting file size is > to 4GiB
+            # https://github.com/vapoursynth/vapoursynth/blob/5b2d5562726a91d9a75441cc4728a90e6c9f4f27/src/common/wave.cpp#L142-L150
+            bytes_per_sample = (node.bits_per_sample + 7) // 8
+            data_size = node.num_samples * node.num_channels * bytes_per_sample
+            # https://github.com/vapoursynth/vapoursynth/blob/5b2d5562726a91d9a75441cc4728a90e6c9f4f27/src/common/wave.cpp#L87
+            w64 = (data_size + 68) > 0xFFFFFFFE
 
-    w64 = any([node.num_channels > 2, node.bits_per_sample > 16, node.num_samples > 44100])
+    ext = ".w64" if w64 else ".wav"
+
+    if outfile is None:
+        outfile = ensure_path(uniquify_path(Path(get_workdir(), f"exported{ext}")), export_audionode)
+    elif w64 and outfile.suffix.lower() == ".wav":
+        outfile = outfile.with_suffix(".w64")
+    elif not outfile.suffix:
+        outfile = outfile.with_suffix(ext)
 
     with outfile.open("wb") as bf, get_render_progress() as progress:
         task = progress.add_task("Rendering audio...", total=node.num_frames)
